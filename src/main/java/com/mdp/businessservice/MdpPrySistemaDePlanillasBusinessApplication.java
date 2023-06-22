9121734d6542eec9940c50f5458b1a7bb764c8c7
alonsofernandz <af5620676@gmail.com>
2023-06-22 16:39:54 -0500

    first commit

---
+++ src/main/java/com/mdp/businessservice/MdpPrySistemaDePlanillasBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.mdp.businessservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdpPrySistemaDePlanillasBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdpPrySistemaDePlanillasBusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdp/businessservice/MdpPrySistemaDePlanillasBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdp.businessservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdpPrySistemaDePlanillasBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
